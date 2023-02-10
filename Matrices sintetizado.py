adbfd644642b44045e97dc9ee6f9a8366bee87b8
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2023-02-10 17:30:28 -0500

    Update Matrices sintetizado.py

--- Matrices sintetizado.py
+++ Matrices sintetizado.py
@@ -1,19 +1,14 @@
 import random
 
 def llenar_matriz_aleatoria(filas, columnas):
-    return [[random.randint(0, 100) for j in range(columnas)] for i in range(filas)]
+    return [[random.randint(0, 100) for _ in range(columnas)] for _ in range(filas)]
 
 def imprimir_matriz(matriz):
-    for fila in matriz:
-        print(" ".join(str(elem) for elem in fila))
+    print(*(" ".join(map(str, fila)) for fila in matriz), sep="\n")
 
 def sumar_elementos(matriz):
     return sum(sum(fila) for fila in matriz)
 
 mi_matriz = llenar_matriz_aleatoria(3, 4)
 imprimir_matriz(mi_matriz)
-print("\n")
-resultado = sumar_elementos(mi_matriz)
-print(f"La suma es: {resultado}")
-print("\n")
-print(mi_matriz)
+print(f"\nLa suma es: {sumar_elementos(mi_matriz)}\n")
