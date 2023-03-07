dd490db0964e2e218529e74ee70282924f0a4ac3
Jairo Andrés Mendoza Villalba <93752506+AndresM412@users.noreply.github.com>
2023-03-06 22:42:20 -0500

    se integra operadores logicos and para preguntas

--- Algoritmo Metodos de conteo/Preguntas.py
+++ Algoritmo Metodos de conteo/Preguntas.py
@@ -1,72 +1,63 @@
 import Formulas
+
 class Preguntas:
-    Question1= "¿En su problema importa el orden?"
-    Question2= "¿Intervienen todos los elementos?"
-    Question3= "¿Se repiten los elementos?"
+    Question1 = "¿En su problema importa el orden?"
+    Question2 = "¿Intervienen todos los elementos?"
+    Question3 = "¿Se repiten los elementos?"
+    
     while True:
-        entrada = input("Hola bienvenido para ingresar al algoritmo pulse la tecla Enter, sí desea salir del programa escriba 'Salir'")
-        if entrada == "":
-            break
-        elif entrada.lower() == "salir":
+        entrada = input("Hola, bienvenido para ingresar al algoritmo pulse la tecla Enter, sí desea salir del programa escriba 'Salir'")
+        if entrada.lower() in ["", "salir"]:
             break
         else:
             print("Entrada no válida")
+
     while True:
-        answer=input(Question1)
-        if answer.lower()=="si":
-            answer=input(Question2)
-            if answer.lower()=="si":
-                answer=input(Question3)
-                if answer.lower()=="si":
-                   print("Permutaciones con repeticion")
-                   break
-                elif answer.lower()=="no":
-                    print("Permutaciones ordinarias\n")
-                    m = int(input("Ingrese el valor de m: "))
-                    Formulas.formulas.permutacion_ordinaria(m)
-                    break
-                else:
-                    print("Por favor ingrese una respuesta")
-            elif answer.lower()=="no":
-                answer=input(Question3)
-                if answer.lower()=="si":
-                    print("Variaciones con repeticion")
-                    m = int(input("Ingrese el valor de m: "))
-                    n = int(input("Ingrese el valor de n: "))
-                    Formulas.formulas.variacion_repite(m,n)
-                    break
-                elif answer.lower()=="no":
-                    print("Variaciones Ordinarias")
-                    m = int(input("Ingrese el valor de m: "))
-                    n = int(input("Ingrese el valor de n: "))
-                    Formulas.formulas.variacion_ordinaria(m,n)
-                    break
-                else:
-                    print("Por favor ingrese una respuesta")
-            else:
-                print("Por favor ingrese una respuesta")
-        elif answer.lower()=="no":
-            answer=input(Question2)
-            if answer.lower()=="no":
-                answer=input(Question3)
-                if answer.lower()== "si":
-                    print("Combinaciones con repeticion")
-                    m = int(input("Ingrese el valor de m: "))
-                    n = int(input("Ingrese el valor de n: "))
-                    Formulas.formulas.combinacion_repite(m,n)
-                    break
-                elif answer.lower()== "no":
-                    print("Combinaciones ordinarias")
-                    m = int(input("Ingrese el valor de m: "))
-                    n = int(input("Ingrese el valor de n: "))
-                    Formulas.formulas.combinacion_ordinaria(m,n)
-                    break
-                else:
-                    print("Por favor ingrese una respuesta")
-            else:
-                print("Por favor ingrese una respuesta")
+        answer1 = input(Question1).lower()
+        answer2 = input(Question2).lower()
+        answer3 = input(Question3).lower()
+        
+        if answer1 == "si" and answer2 == "si" and answer3 == "si":
+            print("Permutaciones con repeticion")
+            w = str(input("Ingrese valor de w: "))
+            break
+            
+        elif answer1 == "si" and answer2 == "si" and answer3 == "no":
+            print("Permutaciones ordinarias\n")
+            m = int(input("Ingrese el valor de m: "))
+            Formulas.formulas.permutacion_ordinaria(m)
+            break
+            
+        elif answer1 == "si" and answer2 == "no" and answer3 == "si":
+            print("Variaciones con repeticion")
+            m = int(input("Ingrese el valor de m: "))
+            n = int(input("Ingrese el valor de n: "))
+            Formulas.formulas.variacion_repite(m, n)
+            break
+            
+        elif answer1 == "si" and answer2 == "no" and answer3 == "no":
+            print("Variaciones Ordinarias")
+            m = int(input("Ingrese el valor de m: "))
+            n = int(input("Ingrese el valor de n: "))
+            Formulas.formulas.variacion_ordinaria(m, n)
+            break
+            
+        elif answer1 == "no" and answer2 == "no" and answer3 == "si":
+            print("Combinaciones con repeticion")
+            m = int(input("Ingrese el valor de m: "))
+            n = int(input("Ingrese el valor de n: "))
+            Formulas.formulas.combinacion_repite(m, n)
+            break
+            
+        elif answer1 == "no" and answer2 == "no" and answer3 == "no":
+            print("Combinaciones ordinarias")
+            m = int(input("Ingrese el valor de m: "))
+            n = int(input("Ingrese el valor de n: "))
+            Formulas.formulas.combinacion_ordinaria(m, n)
+            break
+            
         else:
-            print("Por favor ingrese una respuesta")
+            print("Por favor ingrese una respuesta válida")
         
             
    
